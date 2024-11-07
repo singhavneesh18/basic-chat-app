19e5b0318e813f24a3c3165816de101050fe4aab
Avneesh Kumar <74506226+singhavneesh18@users.noreply.github.com>
2024-11-07 13:21:06 +0000

    Added example ex3, of group/channel chat. (#2)
    * Added ex3 changes
    
    * Final version of ex3
    
    ---------
    
    Co-authored-by: Avneesh Kumar Lenevo <singhavneesh18@gmail.com>

---
+++ example-three/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+const path = require('path')
+
+
+const app = express()
+
+const server = http.createServer(app)
+const io = new Server(server)
+
+const connectionInfo = [];
+io.on('connection', (socket) => {
+    // sockets.push(socket)
+    console.log(`New socket detected on ${socket.id}`)
+
+    socket.on('chat-message', (data) => {
+        console.log("Message received :", data)
+        connectionInfo
+        .filter(con => (con.socket.id != socket.id && con.channelName === data.channelName))
+        .forEach(con => con.socket.emit('chat-message', data));
+    })
+
+    socket.on('createConnection', data => {
+        console.log("New user connected :", data)
+        connectionInfo.push({ userName: data.userName, channelName: data.channelName, socket: socket })
+    })
+
+
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./example-three/public/index.html'))
+})
+
+app.get('/ex3.js', (req, res) => {
+    res.sendFile(path.resolve('./example-three/public/ex3.js'))
+})
+
+
+server.listen(3000)
